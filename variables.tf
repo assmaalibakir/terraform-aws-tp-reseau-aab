ee87b12038f0e5dbc62c261d71db1e15cc620800
Assma <assma@example.com>
2026-09-11 11:57:49 +0200

    Init: provider AWS et variables communes

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "Région de travail"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "identifiant" {
+  description = "Identifiant du stagiaire (remplace NN)"
+  type        = string
+  default     = "ajc1"
+}
